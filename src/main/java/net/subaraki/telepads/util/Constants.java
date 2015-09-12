a0fea6e375f18ad59eb24d910661142ab729ecc7
Darkhax <darklime@live.ca>
2015-09-12 12:51:56 -0600

    Fixed a typo in naming.

--- src/main/java/net/subaraki/telepads/util/Constants.java
+++ src/main/java/net/subaraki/telepads/util/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String VERSION = "1.0.0";
     public static final String SERVER = "net.subaraki.telepads.common.CommonProxy";
     public static final String CLIENT = "net.subaraki.telepads.client.ClientProxy";
-    public static final String FACTORY = "net.subaraki.telepads.client.gui.TelepadsGuiFactor";
+    public static final String FACTORY = "net.subaraki.telepads.client.gui.TelepadsGuiFactory";
     public static final String DEPENDENCY = "required-after:bookshelf";
     public static final Logger LOG = LogManager.getLogger(MOD_NAME);
 }
