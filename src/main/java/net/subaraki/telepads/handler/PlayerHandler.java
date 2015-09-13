0693c716533ca466456b408cf4c07687e322fa75
Darkhax <darklime@live.ca>
2015-09-13 11:11:19 -0600

    Removed test code.

--- src/main/java/net/subaraki/telepads/handler/PlayerHandler.java
+++ src/main/java/net/subaraki/telepads/handler/PlayerHandler.java
@@ -31,22 +31,4 @@ public class PlayerHandler {
         if (event.entity instanceof EntityPlayer && !event.entity.worldObj.isRemote && PlayerLocations.hasProperties((EntityPlayer) event.entity))
             PlayerLocations.getProperties((EntityPlayer) event.entity).sync();
     }
-    
-    @SubscribeEvent
-    public void testEvent (PlayerInteractEvent event) {
-        
-        // Creates an access point to the players locations.
-        PlayerLocations locations = PlayerLocations.getProperties(event.entityPlayer);
-        
-        if (event.action == Action.RIGHT_CLICK_BLOCK) {
-            
-            locations.addEntry(new TelepadEntry(locations.getSuggestedEntryName(), event.entityPlayer.dimension, new Position(event.entityPlayer)));
-            
-            if (event.entityPlayer.worldObj.isRemote)
-                System.out.println("Client: " + locations.getEntries().toString());
-                
-            else
-                System.out.println("Server: " + locations.getEntries().toString());
-        }
-    }
 }
