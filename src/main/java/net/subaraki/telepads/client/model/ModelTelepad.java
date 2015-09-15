de059fa5d08a1cc1bc6452bbd58d4beba6d34b19
Darkhax <darklime@live.ca>
2015-09-15 06:58:48 -0600

    Remapped the model, and separated the arrows and the rest of the base into different render layers.

--- src/main/java/net/subaraki/telepads/client/model/ModelTelepad.java
+++ src/main/java/net/subaraki/telepads/client/model/ModelTelepad.java
@@ -2,110 +2,122 @@ package net.subaraki.telepads.client.model;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.entity.Entity;
 
 public class ModelTelepad extends ModelBase {
-    // fields
-    ModelRenderer shape1;
-    ModelRenderer shape2;
-    ModelRenderer shape3;
-    ModelRenderer shape4;
-    ModelRenderer shape5;
-    ModelRenderer shape6;
-    ModelRenderer shape7;
-    ModelRenderer shape8;
-    ModelRenderer shape9;
-    ModelRenderer shape10;
+    
+    ModelRenderer frame;
+    ModelRenderer arrows;
+    ModelRenderer legWestRight;
+    ModelRenderer legWestLeft;
+    ModelRenderer legEastRight;
+    ModelRenderer legEastLeft;
+    ModelRenderer legSouthRight;
+    ModelRenderer legSouthLeft;
+    ModelRenderer legNorthRight;
+    ModelRenderer legNorthLeft;
     
     public ModelTelepad() {
+        
         textureWidth = 64;
         textureHeight = 32;
         
-        shape1 = new ModelRenderer(this, 0, 0);
-        shape1.addBox(-5F, 0F, -5F, 10, 1, 10);
-        shape1.setRotationPoint(0F, 22F, 0F);
-        shape1.setTextureSize(64, 32);
-        shape1.mirror = true;
-        setRotation(shape1, 0F, 0F, 0F);
-        shape2 = new ModelRenderer(this, 0, 0);
-        shape2.addBox(-3F, 0F, 0F, 3, 1, 2);
-        shape2.setRotationPoint(-5F, 22F, 0F);
-        shape2.setTextureSize(64, 32);
-        shape2.mirror = true;
-        setRotation(shape2, -0.2F, 0F, -0.4F);
-        shape3 = new ModelRenderer(this, 0, 0);
-        shape3.addBox(-3F, 0F, -2F, 3, 1, 2);
-        shape3.setRotationPoint(-5F, 22F, 0F);
-        shape3.setTextureSize(64, 32);
-        shape3.mirror = true;
-        setRotation(shape3, 0.2F, 0F, -0.4F);
-        shape4 = new ModelRenderer(this, 0, 11);
-        shape4.addBox(-5F, -0.5F, -5F, 10, 1, 10);
-        shape4.setRotationPoint(0F, 23F, 0F);
-        shape4.setTextureSize(64, 32);
-        shape4.mirror = true;
-        setRotation(shape4, 0F, 0.8F, 0F);
-        shape5 = new ModelRenderer(this, 0, 0);
-        shape5.addBox(0F, 0F, 0F, 3, 1, 2);
-        shape5.setRotationPoint(5F, 22F, 0F);
-        shape5.setTextureSize(64, 32);
-        shape5.mirror = true;
-        setRotation(shape5, -0.2F, 0F, 0.4F);
-        shape6 = new ModelRenderer(this, 0, 0);
-        shape6.addBox(0F, 0F, -2F, 3, 1, 2);
-        shape6.setRotationPoint(5F, 22F, 0F);
-        shape6.setTextureSize(64, 32);
-        shape6.mirror = true;
-        setRotation(shape6, 0.2F, 0F, 0.4F);
-        shape7 = new ModelRenderer(this, 0, 0);
-        shape7.addBox(-2F, 0F, -3F, 2, 1, 3);
-        shape7.setRotationPoint(0F, 22F, -5F);
-        shape7.setTextureSize(64, 32);
-        shape7.mirror = true;
-        setRotation(shape7, 0.4F, 0.1F, -0.2F);
-        shape8 = new ModelRenderer(this, 0, 0);
-        shape8.addBox(0F, 0F, -3F, 2, 1, 3);
-        shape8.setRotationPoint(0F, 22F, -5F);
-        shape8.setTextureSize(64, 32);
-        shape8.mirror = true;
-        setRotation(shape8, 0.4F, -0.05F, 0.2F);
-        shape9 = new ModelRenderer(this, 0, 0);
-        shape9.addBox(0F, 0F, 0F, 2, 1, 3);
-        shape9.setRotationPoint(0F, 22F, 5F);
-        shape9.setTextureSize(64, 32);
-        shape9.mirror = true;
-        setRotation(shape9, -0.4F, 0.1F, 0.2F);
-        shape10 = new ModelRenderer(this, 0, 0);
-        shape10.addBox(-2F, 0F, 0F, 2, 1, 3);
-        shape10.setRotationPoint(0F, 22F, 5F);
-        shape10.setTextureSize(64, 32);
-        shape10.mirror = true;
-        setRotation(shape10, -0.4F, -0.05F, -0.2F);
+        frame = new ModelRenderer(this, 0, 0);
+        frame.addBox(-5F, 0F, -5F, 10, 1, 10);
+        frame.setRotationPoint(0F, 22F, 0F);
+        frame.setTextureSize(64, 32);
+        frame.mirror = true;
+        setRotation(frame, 0F, 0F, 0F);
+        
+        legWestRight = new ModelRenderer(this, 0, 0);
+        legWestRight.addBox(-3F, 0F, 0F, 3, 1, 2);
+        legWestRight.setRotationPoint(-5F, 22F, 0F);
+        legWestRight.setTextureSize(64, 32);
+        legWestRight.mirror = true;
+        setRotation(legWestRight, -0.2F, 0F, -0.4F);
+        
+        legWestLeft = new ModelRenderer(this, 0, 0);
+        legWestLeft.addBox(-3F, 0F, -2F, 3, 1, 2);
+        legWestLeft.setRotationPoint(-5F, 22F, 0F);
+        legWestLeft.setTextureSize(64, 32);
+        legWestLeft.mirror = true;
+        setRotation(legWestLeft, 0.2F, 0F, -0.4F);
+        
+        arrows = new ModelRenderer(this, 0, 11);
+        arrows.addBox(-5F, -0.5F, -5F, 10, 1, 10);
+        arrows.setRotationPoint(0F, 23F, 0F);
+        arrows.setTextureSize(64, 32);
+        arrows.mirror = true;
+        setRotation(arrows, 0F, 0.8F, 0F);
+        
+        legEastLeft = new ModelRenderer(this, 0, 0);
+        legEastLeft.addBox(0F, 0F, 0F, 3, 1, 2);
+        legEastLeft.setRotationPoint(5F, 22F, 0F);
+        legEastLeft.setTextureSize(64, 32);
+        legEastLeft.mirror = true;
+        setRotation(legEastLeft, -0.2F, 0F, 0.4F);
+        
+        legEastRight = new ModelRenderer(this, 0, 0);
+        legEastRight.addBox(0F, 0F, -2F, 3, 1, 2);
+        legEastRight.setRotationPoint(5F, 22F, 0F);
+        legEastRight.setTextureSize(64, 32);
+        legEastRight.mirror = true;
+        setRotation(legEastRight, 0.2F, 0F, 0.4F);
+        
+        legSouthRight = new ModelRenderer(this, 0, 0);
+        legSouthRight.addBox(-2F, 0F, -3F, 2, 1, 3);
+        legSouthRight.setRotationPoint(0F, 22F, -5F);
+        legSouthRight.setTextureSize(64, 32);
+        legSouthRight.mirror = true;
+        setRotation(legSouthRight, 0.4F, 0.1F, -0.2F);
+        
+        legSouthLeft = new ModelRenderer(this, 0, 0);
+        legSouthLeft.addBox(0F, 0F, -3F, 2, 1, 3);
+        legSouthLeft.setRotationPoint(0F, 22F, -5F);
+        legSouthLeft.setTextureSize(64, 32);
+        legSouthLeft.mirror = true;
+        setRotation(legSouthLeft, 0.4F, -0.05F, 0.2F);
+        
+        legNorthRight = new ModelRenderer(this, 0, 0);
+        legNorthRight.addBox(0F, 0F, 0F, 2, 1, 3);
+        legNorthRight.setRotationPoint(0F, 22F, 5F);
+        legNorthRight.setTextureSize(64, 32);
+        legNorthRight.mirror = true;
+        setRotation(legNorthRight, -0.4F, 0.1F, 0.2F);
+        
+        legNorthLeft = new ModelRenderer(this, 0, 0);
+        legNorthLeft.addBox(-2F, 0F, 0F, 2, 1, 3);
+        legNorthLeft.setRotationPoint(0F, 22F, 5F);
+        legNorthLeft.setTextureSize(64, 32);
+        legNorthLeft.mirror = true;
+        setRotation(legNorthLeft, -0.4F, -0.05F, -0.2F);
     }
     
-    @Override
-    public void render (Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+    public void renderFrameAndLegs (float f5) {
         
-        super.render(entity, f, f1, f2, f3, f4, f5);
-        setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-        render(f5);
+        frame.render(f5);
+        legWestRight.render(f5);
+        legWestLeft.render(f5);
+        legEastRight.render(f5);
+        legEastLeft.render(f5);
+        legSouthRight.render(f5);
+        legSouthLeft.render(f5);
+        legNorthRight.render(f5);
+        legNorthLeft.render(f5);
     }
     
-    public void render (float f5) {
+    public void renderArrows (float f5) {
         
-        shape1.render(f5);
-        shape2.render(f5);
-        shape3.render(f5);
-        shape4.render(f5);
-        shape5.render(f5);
-        shape6.render(f5);
-        shape7.render(f5);
-        shape8.render(f5);
-        shape9.render(f5);
-        shape10.render(f5);
-        
+        arrows.render(f5);
     }
     
+    /**
+     * Sets the rotations for a model piece.
+     * 
+     * @param model: The model to rotate.
+     * @param x: The rotation for the X axis.
+     * @param y: The rotation for the Y axis.
+     * @param z: The rotation for the Z axis.
+     */
     private void setRotation (ModelRenderer model, float x, float y, float z) {
         
         model.rotateAngleX = x;
