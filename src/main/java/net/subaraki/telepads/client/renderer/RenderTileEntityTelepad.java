9b125a07a068c0fefaba65553e06e50303273bbe
ArtixAllMighty <robbeenaxel@hotmail.com>
2015-10-06 21:17:15 +0200

    trying to render a torch on the teleapad

--- src/main/java/net/subaraki/telepads/client/renderer/RenderTileEntityTelepad.java
+++ src/main/java/net/subaraki/telepads/client/renderer/RenderTileEntityTelepad.java
@@ -4,12 +4,18 @@ import java.awt.Color;
 import java.nio.FloatBuffer;
 import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.subaraki.telepads.client.model.ModelTelepad;
@@ -41,6 +47,9 @@ public class RenderTileEntityTelepad extends TileEntitySpecialRenderer {
 	private static ResourceLocation frame_powered_on = new ResourceLocation(
 			"minecraft:textures/blocks/redstone_block.png");
 
+	private static ItemStack torch_on;
+	private static ItemStack torch_off;
+
 	private static int animation_counter;
 
 	// copied from RenderEndPortal.java
@@ -52,6 +61,14 @@ public class RenderTileEntityTelepad extends TileEntitySpecialRenderer {
 
 	FloatBuffer field_76908_a = GLAllocation.createDirectFloatBuffer(16);
 	ModelTelepad padModel = new ModelTelepad();
+
+	RenderBlocks renderBlocks;
+
+	public RenderTileEntityTelepad() {
+		renderBlocks = new RenderBlocks();
+		torch_on = new ItemStack(Blocks.redstone_torch, 1);
+		torch_off = new ItemStack(Blocks.unlit_redstone_torch, 1);
+	}
 
 	private FloatBuffer func_76907_a(float par1, float par2, float par3,
 			float par4) {
@@ -195,8 +212,7 @@ public class RenderTileEntityTelepad extends TileEntitySpecialRenderer {
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
-		GL11.glTranslatef((float) d + 0.5F, (float) d1 + 2.25F,
-				(float) d2 + 0.5F);
+		GL11.glTranslatef((float) d + 0.5F, (float) d1 + 2.25F, (float) d2 + 0.5F);
 		GL11.glScalef(1.0F, -1F, -1F);
 
 		float f2 = 1.5f;
@@ -204,38 +220,23 @@ public class RenderTileEntityTelepad extends TileEntitySpecialRenderer {
 
 		GL11.glPushMatrix();
 		bindTexture(base);
-		GL11.glColor3f((float) (colorBase.getRed() / 255.0f),
-				(float) (colorBase.getGreen() / 255.0f),
-				(float) (colorBase.getBlue() / 255.0f));
+		GL11.glColor3f((float) (colorBase.getRed() / 255.0f),(float) (colorBase.getGreen() / 255.0f),(float) (colorBase.getBlue() / 255.0f));
 		padModel.renderArrows(0.0625f);
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
 		bindTexture(pads);
-		GL11.glColor3f((float) (colorFrame.getRed() / 255.0f),
-				(float) (colorFrame.getGreen() / 255.0f),
-				(float) (colorFrame.getBlue() / 255.0f));
+		GL11.glColor3f((float) (colorFrame.getRed() / 255.0f),(float) (colorFrame.getGreen() / 255.0f),(float) (colorFrame.getBlue() / 255.0f));
 		padModel.renderLegs(0.0625f);
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
-
 		GL11.glColor3f(1f, 1f, 1f);
-		// TODO this is to be removed, and just redstone particle effects should
-		// be added !
-		// or a redstone lamp to be rendered on the side
-		if (te.hasRedstoneUpgrade()) {
-			if (te.isPowered())
-				bindTexture(frame_powered_on);
-			else
-				bindTexture(frame_powered_off);
-		} else
-			bindTexture(frame);
-
+		bindTexture(frame);
 		padModel.renderFrame(0.0625f);
 		GL11.glPopMatrix();
 
-		if (te.hasDimensionUpgrade()) {
+		if (te.hasDimensionUpgrade()){
 
 			GL11.glPushMatrix();
 
@@ -282,13 +283,26 @@ public class RenderTileEntityTelepad extends TileEntitySpecialRenderer {
 			GL11.glPopMatrix();
 		}
 
-		GL11.glPopMatrix();
+		if (te.hasRedstoneUpgrade()) {
 
-		// System.out.println("The color is: " + colorBase.toString());
+//			GL11.glPushMatrix();
+
+			if (te.isPowered()){
+//				renderStacks(torch_on, 1f);
+				renderTorch(Blocks.redstone_torch, te.xCoord, te.yCoord, te.zCoord);
+			}
+			else{
+//				renderStacks(torch_off, 1f);
+				renderTorch(Blocks.unlit_redstone_torch, te.xCoord, te.yCoord, te.zCoord);
+			}
+
+//			GL11.glPopMatrix();
+		}
+
+		GL11.glPopMatrix();
 	}
 
-	public void renderInventory(TileEntity tileentity, Color colorFrame,
-			Color colorBase, double d, double d1, double d2, float f) {
+	public void renderInventory(TileEntity tileentity, Color colorFrame,Color colorBase, double d, double d1, double d2, float f) {
 
 		animation_counter++;
 
@@ -302,8 +316,7 @@ public class RenderTileEntityTelepad extends TileEntitySpecialRenderer {
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
-		GL11.glTranslatef((float) d + 0.5F, (float) d1 + 2.25F,
-				(float) d2 + 0.5F);
+		GL11.glTranslatef((float) d + 0.5F, (float) d1 + 2.25F,(float) d2 + 0.5F);
 		GL11.glScalef(1.0F, -1F, -1F);
 
 		float f2 = 1.5f;
@@ -311,28 +324,98 @@ public class RenderTileEntityTelepad extends TileEntitySpecialRenderer {
 
 		GL11.glPushMatrix();
 		bindTexture(base);
-		GL11.glColor3f((float) (colorBase.getRed() / 255.0f),
-				(float) (colorBase.getGreen() / 255.0f),
-				(float) (colorBase.getBlue() / 255.0f));
+		GL11.glColor3f((float) (colorBase.getRed() / 255.0f),(float) (colorBase.getGreen() / 255.0f),(float) (colorBase.getBlue() / 255.0f));
 		padModel.renderArrows(0.0625f);
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
 		bindTexture(pads);
-		GL11.glColor3f((float) (colorFrame.getRed() / 255.0f),
-				(float) (colorFrame.getGreen() / 255.0f),
-				(float) (colorFrame.getBlue() / 255.0f));
+		GL11.glColor3f((float) (colorFrame.getRed() / 255.0f), (float) (colorFrame.getGreen() / 255.0f), (float) (colorFrame.getBlue() / 255.0f));
 		padModel.renderLegs(0.0625f);
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
-
 		bindTexture(frame);
-
 		GL11.glColor3f(1f, 1f, 1f);
 		padModel.renderFrame(0.0625f);
 		GL11.glPopMatrix();
 
 		GL11.glPopMatrix();
+	}
+
+	private void renderStacks(ItemStack itemstack, float scale) {
+
+		GL11.glPushMatrix();
+		if ((itemstack != null) && (itemstack.getItem() != null)) {
+			bindTextureMap(itemstack);
+
+			/* =======BLOCKS======= */
+			if (itemstack.getItemSpriteNumber() == 0 && itemstack.getItem() instanceof ItemBlock &&	RenderBlocks.renderItemIn3d(Block.getBlockFromItem(itemstack.getItem()).getRenderType())){
+				GL11.glTranslatef(0.25f, 0.44f, -0.25f);
+
+				float f2 = 0.25F;
+				int i1 = Block.getBlockFromItem(itemstack.getItem()).getRenderType();
+
+				if ((i1 == 1) || (i1 == 19) || (i1 == 12) || (i1 == 2))
+					f2 = 0.5F;
+
+				if (scale > 0f) 
+					f2 = 0.2f;
+
+				int b0 = 1;
+				if (itemstack.stackSize > 1) 
+					b0 = 2;
+
+				if (itemstack.stackSize > 20) 
+					b0 = 3;
+
+				if (itemstack.stackSize > 40) 
+					b0 = 4;
+
+				GL11.glScalef(f2, f2, f2);
+
+				//translate
+
+				for (int i = 0; i < b0; i++) {
+					GL11.glTranslatef(0.05f * i, 0.05f * i, 0.05f * i);
+					renderBlocks.renderBlockAsItem(Block.getBlockFromItem(itemstack.getItem()), itemstack.getItemDamage(), 1.0f);
+				}
+			}
+			/* =====ITEMS===== */
+			else {
+				//Currently no need for item rendering. i'll leave this in in case of
+				//				//translate here
+				//				try {
+				//					if (itemstack.getItem().requiresMultipleRenderPasses()) {
+				//						for (int k = 0; k <= 1; k++)
+				//							drawItem(itemstack, k);
+				//					}
+				//					else {
+				//						drawItem(itemstack, 0);
+				//					}
+				//				}
+				//
+				//				catch (Throwable throwable) {
+				//					throw new RuntimeException(throwable);
+				//				}
+			}
+		}
+		GL11.glPopMatrix();
+	}
+
+	private void bindTextureMap(ItemStack item) {
+
+		Minecraft.getMinecraft().renderEngine.bindTexture(RenderManager.instance.renderEngine.getResourceLocation(item.getItemSpriteNumber()));
+	}
+	
+	private void renderTorch(Block block, int x, int y, int z){
+        Tessellator tessellator = Tessellator.instance;
+        tessellator.setBrightness(15); //Minecraft.getMinecraft().theWorld.getMixedBrightnessForBlock(Minecraft.getMinecraft(), x, y, z));
+        tessellator.setColorOpaque_F(1.0F, 1.0F, 1.0F);
+        double d0 = 0.4000000059604645D;
+        double d1 = 0.5D - d0;
+        double d2 = 0.20000000298023224D;
+        
+        renderBlocks.renderTorchAtAngle(block, (double)x, (double)y, (double)z, 0.0D, 0.0D, 0);
 	}
 }
